f1b38ac4b8ca36b23963389e22e1e624b5e3eea1
dahee0325 <43061530+dahee0325@users.noreply.github.com>
2019-07-29 18:26:24 +0900

    0729 Spring ...

---
+++ daheeSpring/MemberManagerJAVA/src/member/MemberRegisterService.java
@@ -0,0 +1,26 @@
+package member;
+
+import java.util.Date;
+
+public class MemberRegisterService {
+	
+	private MemberDao memberDao;
+	
+	public MemberRegisterService(MemberDao memberDao) {
+		this.memberDao = memberDao;
+	}
+	
+	public void regist(RegisterRequest request) throws AlreadyExistingMemberException {
+		
+		Member member = memberDao.selectByEmail(request.getEmail());
+		
+		if(member != null) {
+			//같은 이메일이 등록되어있다는 뜻
+			throw new AlreadyExistingMemberException();
+		}
+		
+		Member newMember = new Member(request.getEmail(), request.getPassword(), request.getName(), new Date());
+		
+		memberDao.insert(newMember);
+	}
+}
