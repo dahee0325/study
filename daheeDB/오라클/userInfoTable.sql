dd274ede38bf152a73f09ae2b5083638981e7fcd
dahee0325 <43061530+dahee0325@users.noreply.github.com>
2019-08-12 18:06:19 +0900

    Update userInfoTable.sql

--- daheeDB/오라클/userInfoTable.sql
+++ daheeDB/오라클/userInfoTable.sql
@@ -7,11 +7,12 @@ create table guestbook_message (
  message_id number not null primary key,
  guest_name varchar2(50) not null,
  password varchar2(10) not null,
- message long not null
+ message varchar(4000) not null
 );
 
 select * from guestbook_message;
-
+drop table guestbook_message;
+drop sequence gm_mid_seq;
 
 -- mvc_member project sql
 create table userInfo (
